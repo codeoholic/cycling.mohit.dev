2b4deaa7dde11acafbc4d4e6a357977a76e2354c
codeoholic <mohit.tyagi@protonmail.com>
2020-08-01 02:24:25 +0530

    feat:publishing strava statistics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { total_rides : 0, total_distance : 0 };
+    }
+    componentDidMount(){
+        const url = 'https://api-cycling.mohit.dev/record.php';
+        fetch( url )
+        .then(response => response.json())
+        .then(responseJson => {
+            this.setState({ total_rides: responseJson.all_ride_totals_count, total_distance: responseJson.all_ride_totals_distance });
+        })
+    }
+    render(){
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src="https://mohit.sgp1.cdn.digitaloceanspaces.com/cycling/cycling.png" className="cycling_icon" alt="cycling icon"/>
+                    <div className="statistics">{this.state.total_rides} rides</div>
+                    <div className="statistics">{this.state.total_distance / 1000} kilometers</div>
+                </header>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default ( App );
